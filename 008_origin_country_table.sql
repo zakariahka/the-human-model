d4062b89fb633b31e42532becc1c3fbe9527f902
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-12-14 13:35:56 -0500

    changing nationality to origin country since person cant change it

--- 008_origin_country_table.sql
+++ 008_origin_country_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE nationality (
+CREATE TABLE origin_country (
 	person_id UUID NOT NULL REFERENCES person(person_id),
 	country_id UUID NOT NULL REFERENCES country(country_id),
 	PRIMARY KEY (person_id, country_id)

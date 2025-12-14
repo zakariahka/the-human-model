86a0b11bd288e0142835eef608fade2cc8dc9a5a
zakariahka <102551487+zakariahka@users.noreply.github.com>
2025-12-14 13:31:50 -0500

    making nationality to be consistent

--- 008_nationality_table.sql
+++ 008_nationality_table.sql
@@ -1,8 +1,5 @@
 CREATE TABLE nationality (
 	person_id UUID NOT NULL REFERENCES person(person_id),
 	country_id UUID NOT NULL REFERENCES country(country_id),
-	start_date timestamp NOT NULL,
-	end_date timestamp,
-	PRIMARY KEY (person_id, country_id, start_date),
-	CHECK(end_date is NULL OR end_date >= start_date)
+	PRIMARY KEY (person_id, country_id)
 );
